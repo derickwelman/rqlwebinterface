1cc604420e085cdfef10cad3e6f58f2b44f4b053
pliavi <pliavi.rpgm@gmail.com>
2016-11-05 13:13:10 -0200

    Fixing idFavo from Abelha
    Abelha's relvar had "idCom" on it's FK for Favo's relvar rather than "IdFavo". Implicating that you can't Join a bee table or Rename it's columns.

--- pt/sql/Separated parts/BeehivePostgres-portuguese.sql
+++ pt/sql/Separated parts/BeehivePostgres-portuguese.sql
@@ -284,4 +284,4 @@ Flor.posicaoY:INTEGER;
 Polen.idFlor:INTEGER;
 Polen.idAbelha:INTEGER;
 
-Colmeia.idColmeia:INTEGER;Colmeia.posicaoX:INTEGER;Colmeia.posicaoY:INTEGER;Colmeia.mel:DOUBLE;Favo.idFavo:INTEGER;Favo.idColmeia:INTEGER;Trabalho.idTrabalho:INTEGER;Trabalho.descricao:VARCHAR;Trabalho.salario:DOUBLE;Abelha.idAbelha:INTEGER;Abelha.nome:VARCHAR;Abelha.idade:INTEGER;Abelha.mel:DOUBLE;Abelha.idTrabalho:INTEGER;Abelha.idCom:INTEGER;Hierarquia.idInferior:INTEGER;Hierarquia.idSuperior:INTEGER;Flor.idFlor:INTEGER;Flor.posicaoX:INTEGER;Flor.posicaoY:INTEGER;Polen.idFlor:INTEGER;Polen.idAbelha:INTEGER;
+Colmeia.idColmeia:INTEGER;Colmeia.posicaoX:INTEGER;Colmeia.posicaoY:INTEGER;Colmeia.mel:DOUBLE;Favo.idFavo:INTEGER;Favo.idColmeia:INTEGER;Trabalho.idTrabalho:INTEGER;Trabalho.descricao:VARCHAR;Trabalho.salario:DOUBLE;Abelha.idAbelha:INTEGER;Abelha.nome:VARCHAR;Abelha.idade:INTEGER;Abelha.mel:DOUBLE;Abelha.idTrabalho:INTEGER;Abelha.idFavo:INTEGER;Hierarquia.idInferior:INTEGER;Hierarquia.idSuperior:INTEGER;Flor.idFlor:INTEGER;Flor.posicaoX:INTEGER;Flor.posicaoY:INTEGER;Polen.idFlor:INTEGER;Polen.idAbelha:INTEGER;
